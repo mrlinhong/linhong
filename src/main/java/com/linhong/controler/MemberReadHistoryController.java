2b461e172cc8e3d14751c8c367473ae22d309487
linhong <linhong@do1.com.cn>
2020-01-14 18:12:58 +0800

    引入mongodb数据库

---
+++ src/main/java/com/linhong/controler/MemberReadHistoryController.java
@@ -0,0 +1,54 @@
+package com.linhong.controler;
+
+import com.linhong.common.CommonResult;
+import com.linhong.nosql.mongodb.document.MemberReadHistory;
+import com.linhong.service.MemberReadHistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * 会员商品浏览记录管理Controller
+ * Created by macro on 2018/8/3.
+ */
+@Controller
+@RequestMapping("/member/readHistory")
+public class MemberReadHistoryController {
+    @Autowired
+    private MemberReadHistoryService memberReadHistoryService;
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult create(@RequestBody MemberReadHistory memberReadHistory) {
+        int count = memberReadHistoryService.create(memberReadHistory);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult delete(@RequestParam("ids") List<String> ids) {
+        int count = memberReadHistoryService.delete(ids);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<List<MemberReadHistory>> list(Long memberId) {
+        List<MemberReadHistory> memberReadHistoryList = memberReadHistoryService.list(memberId);
+        return CommonResult.success(memberReadHistoryList);
+    }
+}
